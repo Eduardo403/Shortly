20cd9ce13f7a7ef805485ff33ffe0a5d1df3ea91
Eduardo <edua.24h@gmail.com>
2022-07-16 16:18:24 -0400

    remasterizacion de codigo,js de la api y el boton de copiar terminado

--- scrit/copi_text.js
+++ scrit/copi_text.js
@@ -1,20 +1,16 @@
+/**funcion para boton de copiar url-short **/
 export function buttonCopy() {
   const d = document;
-
   const $buttonCopy = d.getElementById("copy-button");
 
   $buttonCopy.addEventListener("click", (e) => {
-    e.preventDefault();
-    // alert("prueba");
-    let $shorUrl = d.getElementById("copy").textContent;
+    let $shorUrl = d.getElementById("copy-short-url").textContent;
     navigator.clipboard.writeText($shorUrl).then(
       (success) => {
         $buttonCopy.textContent = `Copied!`;
         $buttonCopy.style.backgroundColor = "var(--Dark-Violet)";
       },
-      (err) => console.log("error copying text")
+      (err) => console.log(`error copying text:${err}`)
     );
-
-    console.log($shorUrl);
   });
 }
--- scrit/get_api.js
+++ scrit/get_api.js
@@ -1,16 +1,17 @@
 import { buttonCopy } from "./copi_text.js";
-
+/* funcion para acortart url opteniendo de api extena */
 export function getApi() {
-  const d = document;
-  const $root = d.getElementById("root-url");
-  const $from = d.getElementById("from-url");
-  const $loadin = d.querySelector(".loadin");
-  const $submit = d.getElementById("submit");
+  /*constantes uso externo de el Dom */
+  const d = document,
+    $root = d.getElementById("root-url"),
+    $from = d.getElementById("from-url"),
+    $loadin = d.querySelector(".loadin");
 
   $from.addEventListener("submit", async (e) => {
     e.preventDefault();
 
     try {
+      /*valiavles para peticion fetch */
       let templateUrl = "";
       let url = e.target.url.value;
       let apiUrl = `https://api.shrtco.de/v2/shorten?url=${url}`;
@@ -18,28 +19,29 @@ export function getApi() {
       let res = await fetch(apiUrl);
       let json = await res.json();
 
-      console.log(json);
-
-      if (res.ok) {
+      /*insercion en el dom y ejecucion de el boton de copiar */
+      if (!res.ok) {
+        throw { status: res.status, statusText: res.statusText };
+      } else {
         templateUrl = `
         
           <div class="conteiner-url">
             <a href="${json.result.original_link}">${json.result.original_link}</a>
             <hr />
-            <a id="copy" href="${json.result.short_link}">${json.result.short_link}</a>
+            <a id="copy-short-url" href="${json.result.short_link}">${json.result.short_link}</a>
             <button id="copy-button">Copy</button>
           </div>
         
           `;
-
         $root.innerHTML += templateUrl;
         buttonCopy();
       }
 
       $loadin.style.display = "none";
     } catch (err) {
-      let messger = err.statusText || "Ocurio un Error";
-      $root.innerHTML = +`<p>Error:${messger}</p>`;
+      /*programacion en caso de Error  */
+      let messger = err.statusText || " Error al obtener los datos";
+      $root.innerHTML += `<p class="error">Error:${err.status}:${messger}</p>`;
       console.log(err);
       $loadin.style.display = "none";
     }
